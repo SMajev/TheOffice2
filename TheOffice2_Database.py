42f00c8ef410509024da2f1904d3036e2aaa4ce3
smajev <smajev@gmail.com>
2021-10-01 19:59:08 +0200

    ass edit funcs

--- TheOffice2_Database.py
+++ TheOffice2_Database.py
@@ -49,8 +49,6 @@ class Database:
         __tablename__ = 'departments'
         department_id = Column(Integer, primary_key=True, autoincrement=True)
         dep_name = Column(String(30), nullable=False)
-        from_date = Column(Date, nullable=True)
-        to_date = Column(Date, nullable=True)
 
         emp = relationship('Employees', back_populates='department')
         titles = relationship('Titles', back_populates='department')
@@ -59,13 +57,13 @@ class Database:
         __tablename__ = 'titles'
         title_id = Column(Integer, primary_key=True, autoincrement=True)
         title_name = Column(String(30), nullable=True)
-        from_date = Column(String(30), nullable=True)
-        to_date = Column(String(30), nullable=True)
+
 
         emp = relationship('Employees', back_populates='title')
 
         department_id = Column(Integer(), ForeignKey('departments.department_id'), nullable=True)
         department = relationship('Departments', back_populates='titles')
+
 
     def __init__(self):
         self.engine = create_engine('mysql://root:JupyNote1234!@localhost:3306/theoffice2')
@@ -73,9 +71,11 @@ class Database:
         Session = sessionmaker(bind=self.engine)
         self.session = Session()
 
+
     def _add_object(self, object):
         self.session.add(object)
         self.session.commit()
+
 
     def add_employe(self, name: str, surname: str, pesel: str,
                     birth_date=None, hire_date=None, department_id=None, title_id=None):
@@ -86,6 +86,102 @@ class Database:
             salary_id=self._add_salary(), department_id=department_id, title_id=title_id
         )        
         self._add_object(new_employe)
+
+
+    def add_department(self, dep_name):
+        new_department = self.Departments(dep_name=dep_name)
+        self._add_object(new_department)
+
+
+    def add_title(self, title_name, department_id):
+        new_title = self.Titles(title_name=title_name, department_id=department_id)
+        self._add_object(new_title)
+
+
+    def edit_emp_name(self, emp_id, new_name):
+        with self.session.begin():
+            employe = self._search_by_id(emp_id)
+            employe.emp_name = new_name
+
+
+    def edit_emp_surname(self, emp_id, new_surname):
+        with self.session.begin():
+            employe = self._search_by_id(emp_id)
+            employe.emp_surname = new_surname
+
+
+    def edit_emp_pesel(self, emp_id, new_pesel):
+        with self.session.begin():
+            employe = self._search_by_id(emp_id)
+            employe.pesel = new_pesel
+
+
+    def edit_emp_pesel(self, emp_id, new_pesel):
+        with self.session.begin():
+            employe = self._search_by_id(emp_id)
+            employe.pesel = new_pesel
+
+
+    def edit_emp_birth_date(self, emp_id, new_birth_date):
+        with self.session.begin():
+            employe = self._search_by_id(emp_id)
+            employe.birth_date = new_birth_date
+
+
+    def edit_emp_hire_date(self, emp_id, new_hire_date):
+        with self.session.begin():
+            employe = self._search_by_id(emp_id)
+            employe.hire_date = new_hire_date
+
+
+    def edit_emp_department_id(self, emp_id, new_department_id):
+        with self.session.begin():
+            employe = self._search_by_id(emp_id)
+            employe.department_id = new_department_id
+
+
+    def edit_emp_title_id(self, emp_id, new_title_id):
+        with self.session.begin():
+            employe = self._search_by_id(emp_id)
+            employe.department_id = new_title_id
+
+
+    def edit_phone_number(self, emp_id, new_phone_number):
+        with self.session.begin():
+            contact = self._search_contact_by_emp_id(emp_id)
+            contact.phone_number = new_phone_number
+
+    def edit_mail(self, emp_id, new_email):
+        with self.session.begin():
+            contact = self._search_contact_by_emp_id(emp_id)
+            contact.email = new_email
+
+    def edit_street_and_no(self, emp_id, new_adress):
+        with self.session.begin():
+            contact = self._search_contact_by_emp_id(emp_id)
+            contact.street_and_no = new_adress
+
+    def edit_flat_number(self, emp_id, new_flat_number):
+        with self.session.begin():
+            contact = self._search_contact_by_emp_id(emp_id)
+            contact.flat_number = new_flat_number
+
+    def edit_dept_name(self, dep_id, new_dept_name):
+        with self.session.begin():
+            dept = self._search_dept_by_id(dep_id)
+            dept.dep_name = new_dept_name
+
+    def edit_title_name(self, title_id, new_title_name):
+        with self.session.begin():
+            title = self._search_title_by_id(title_id)
+            title.name = new_title_name
+
+    def edit_title_dept(self, title_id, new_dept_id):
+        with self.session.begin():
+            title = self._search_title_by_id(title_id)
+            title.department_id = new_dept_id
+
+
 
     def _add_salary(self, hours=0, per_hour=0, full_salary=0):
         new_salary = self.Salary(hours=hours, per_hour=per_hour, full_salary=full_salary)
@@ -98,4 +194,24 @@ class Database:
         self._add_object(new_contact)
         return new_contact.contact_id
 
+    def _search_by_id(self, id_to_find):        
+        employe = (self.session.query(self.Employees)
+                  .filter(self.Employees.emp_id == id_to_find).first())
+        return employe
 
+    def _search_contact_by_emp_id(self, id_to_find):    
+        contact_id = self._search_by_id(id_to_find).contact_id
+        print(contact_id)
+        contact = (self.session.query(self.Contacts)
+                  .filter(self.Contacts.contact_id == contact_id).first())
+        return contact
+
+    def _search_dept_by_id(self, id_to_find):
+        dept = (self.session.query(self.Departments)
+               .filter(self.Departments.department_id == id_to_find).first())
+        return dept
+
+    def _search_title_by_id(self, id_to_find):
+        title = (self.session.query(self.Titles)
+               .filter(self.Title.title_id == id_to_find).first())
+        return title
