d2d3f364e90e333e35ac6f148d4d60daa5edd12a
smajev <smajev@gmail.com>
2021-10-02 02:46:00 +0200

    ass edit funcs

--- TheOffice2_Database.py
+++ TheOffice2_Database.py
@@ -5,6 +5,9 @@ from sqlalchemy import (
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
+import csv
+import pandas as pd
+
 
 class Database:
     BASE = declarative_base()
@@ -26,6 +29,9 @@ class Database:
         title_id = Column(Integer(), ForeignKey('titles.title_id'))
         title = relationship('Titles', back_populates='emp')
 
+        def __repr__(self):
+            return f'\n\n\nID: {self.emp_id}, Name: {self.emp_name}, Surname: {self.emp_surname},\n       Pesel:{self.pesel}\n\n\n'
+
     class Contacts(BASE):
         __tablename__ = 'contacts'
         contact_id = Column(Integer, primary_key=True, autoincrement=True)
@@ -43,6 +49,8 @@ class Database:
         per_hour = Column(Integer)
         full_salary = Column(Integer)
         emp = relationship('Employees', back_populates='salary')
+        def __repr__(self):
+            return f'salary_id'
 
 
     class Departments(BASE):
@@ -98,12 +106,50 @@ class Database:
         self._add_object(new_title)
 
 
-
-
     def edit_emp_name(self, emp_id: int, new_name: str):
         with self.session.begin():
             employe = self._search_by_id(emp_id)
             employe.emp_name = new_name
+
+
+    def add_hour_to_salary(self, emp_id: int, number_of_hours: int):
+        with self.session.begin():
+            salary = self._search_salary_by_emp_id(emp_id)
+            salary.hours += number_of_hours
+
+
+    def edit_per_hour_value(self, emp_id: int, value):
+        with self.session.begin():
+            salary = self._search_salary_by_emp_id(emp_id)
+            salary.per_hour = value
+
+
+    def show_all_employees(self):
+        df = self._get_data_frame('employees', 'emp_id')
+        print(df)
+
+
+    def monthly_reset(self):
+        with self.session.begin():
+            salaries = self._get_every_salary()
+            print(salaries)
+            self.save_data_to_csv('salaries', 'full_salary.csv', 'salary_id')
+            for salary in salaries:
+                print(salary)
+                salary.hours = 0
+                salary.full_salary = 0
+
+
+    def count_full_salary_for_everyone(self):        
+        with self.session.begin():
+            salaries = self._get_every_salary()
+            for salary in salaries:
+                salary.full_salary = salary.hours * salary.per_hour
+
+
+    def save_data_to_csv(self,  table_name, file_name, idx):
+        df = self._get_data_frame(table_name, idx)
+        df.to_csv(file_name)
 
 
     def edit_emp_surname(self, emp_id: int, new_surname: str):
@@ -183,7 +229,24 @@ class Database:
             title = self._search_title_by_id(title_id)
             title.department_id = new_dept_id
 
+    def delete_employee(self, emp_id):
+        with self.session.begin():
+            try:
+                self.session.delete(self._search_salary_by_emp_id(emp_id))
+                self.session.delete(self._search_contact_by_emp_id(emp_id))
+                self.session.delete(self._search_by_id(emp_id))
+            except:
+                print('Nie ma takiego rekordu')
 
+
+    def _get_data_frame(self, table_name, idx):
+        df = pd.read_sql_table(table_name, self.engine, index_col=idx)
+        return df
+
+
+    def _get_every_salary(self):
+        salaries = self.session.query(self.Salary).all()
+        return salaries
 
 
     def _add_salary(self, hours=0, per_hour=0, full_salary=0):
@@ -191,28 +254,50 @@ class Database:
         self._add_object(new_salary)
         return new_salary.salary_id
 
+
     def _add_contact(self, phone_number=None, email=None, street_and_no=None, flat_number=None):
         new_contact = self.Contacts(phone_number=phone_number, email=email,
                                     street_and_no=street_and_no, flat_number=flat_number)
         self._add_object(new_contact)
         return new_contact.contact_id
 
-    def _search_by_id(self, id_to_find: int):        
-        employe = (self.session.query(self.Employees)
-                  .filter(self.Employees.emp_id == id_to_find).first())
-        return employe
 
-    def _search_contact_by_emp_id(self, id_to_find: int):    
-        contact_id = self._search_by_id(id_to_find).contact_id
-        print(contact_id)
-        contact = (self.session.query(self.Contacts)
-                  .filter(self.Contacts.contact_id == contact_id).first())
-        return contact
+    def _search_by_id(self, id_to_find: int):
+        try:        
+            employe = (self.session.query(self.Employees)
+                    .filter(self.Employees.emp_id == id_to_find).first())
+            
+            return employe
+        except:
+            print('Niepoprawne ID')
+                  
+
+
+    def _search_contact_by_emp_id(self, id_to_find: int):
+        try:    
+            contact_id = self._search_by_id(id_to_find).contact_id
+            contact = (self.session.query(self.Contacts)
+                    .filter(self.Contacts.contact_id == contact_id).first())
+            return contact
+        except:
+            print('Niepoprawne ID')
+
+
+    def _search_salary_by_emp_id(self, id_to_find: int):  
+        try:
+            salary_id = self._search_by_id(id_to_find).salary_id
+            salary = (self.session.query(self.Salary)
+                    .filter(self.Salary.salary_id == salary_id
+                    ).first())
+            return salary
+        except:
+            print('Niepoprawne ID')
 
     def _search_dept_by_id(self, id_to_find: int):
         dept = (self.session.query(self.Departments)
                .filter(self.Departments.department_id == id_to_find).first())
         return dept
+
 
     def _search_title_by_id(self, id_to_find: int):
         title = (self.session.query(self.Titles)
